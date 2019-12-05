405a6c65e28116abe930c3eeec07c08ff204a12b
Ayush Nigam <ayushnigamsworld@gmail.com>
2019-12-05 19:35:46 +0530

    Save Widgets

--- main.js
+++ main.js
@@ -1,12 +1,44 @@
 const angularApp = angular.module("widgetCreation", []);
 angularApp.controller("widgetCtrl", function ($scope, DataService, $http) {
 
+        debugger;
+        const aa = $http.get('https://dashboard.slate-platform.com/api/public/dashing/tables').then(function(dataReceived){
+            $scope.tableDatas = dataReceived.data.data;
+        });
+
+        let selectedChart = '';
+        $scope.saveWidget = function() {
+            debugger;
+            const widgetName = $scope.widgetName;
+            const selectedTableId = $scope.selectedTableName;
+            const selectedApi = $scope.selectedApiUrl;
+            let finalApi = '';
+            if ($scope.selectedTableName) {
+                finalApi += `https://dashboard.slate-platform.com/api/workspace/table/${selectedTableId}`;
+            } else {
+                finalApi += selectedApi;
+            }
+            const dataToPost = {
+                api: finalApi,
+                name: widgetName,
+                chart: selectedChart,
+                charteventscript: '()=> {}',
+                datasource: '()=> {}'
+            };
+            console.log(dataToPost);
+            $http.post('https://dashboard.slate-platform.com/api/public/dashing/widget', dataToPost).then(function(data){
+                console.log('SuccessFul Widget Creation: ', data);
+            });
+        }
+
+
         $scope.onCardClick = function (type) {
             var dom = document.getElementById('container');
             var chart = echarts.getInstanceByDom(dom);
             if(!chart){
                 echarts.init(dom);
             }
+            selectedChart = type;
             if(type == 'pie'){
                 chart.setOption(DataService.pieChart.options());
             } else if(type == 'funnel'){
